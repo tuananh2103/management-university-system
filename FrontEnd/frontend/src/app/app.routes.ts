9061e22ed862fa005e55d56e65ccbff4799e41b3
tuananh2103 <tuananh21039119@gmail.com>
2025-11-15 16:31:26 +0100

    add home, login , students, courses in front

--- FrontEnd/frontend/src/app/app.routes.ts
+++ FrontEnd/frontend/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { HomeComponent } from './pages/home/home';
+import { StudentsComponent } from './pages/students/students';
+import { CoursesComponent } from './pages/courses/courses';
+import { LoginComponent } from './pages/login/login';
+
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'students', component: StudentsComponent },
+  { path: 'courses', component: CoursesComponent },
+  { path: 'login', component: LoginComponent },
+];
---
+++ FrontEnd/frontend/src/app/pages/courses/courses.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-courses',
+  imports: [],
+  templateUrl: './courses.html',
+  styleUrl: './courses.scss',
+})
+export class Courses {
+
+}
