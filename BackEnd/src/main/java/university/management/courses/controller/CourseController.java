fcbc612c6405f1b2a2d47eec117af5476b767288
tuananh2103 <tuananh21039119@gmail.com>
2026-05-25 10:48:21 +0200

    Add Spring Boot courses API

--- BackEnd/src/main/java/university/management/courses/controller/CourseController.java
+++ BackEnd/src/main/java/university/management/courses/controller/CourseController.java
@@ -1,5 +1,29 @@
 package university.management.courses.controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import university.management.courses.dto.CourseDto;
+import university.management.courses.service.CourseService;
+
+@RestController
+@RequestMapping("/api/courses")
+@CrossOrigin(origins = "http://localhost:4200")
 public class CourseController {
-	
+
+    private final CourseService courseService;
+
+    public CourseController(CourseService courseService) {
+        this.courseService = courseService;
+    }
+
+    @GetMapping
+    public List<CourseDto> getCourses(@RequestParam int semester) {
+        return courseService.getCoursesBySemester(semester);
+    }
 }
